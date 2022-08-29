d48f430e66597017aba92da6927902b16da0c1ca
Firok <firok@qq.com>
2022-08-29 13:23:08 +0800

    3.8.0

---
+++ src/main/java/firok/topaz/RegexPipeline.java
@@ -0,0 +1,57 @@
+package firok.topaz;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Pattern;
+
+/**
+ * @since 3.8.0
+ * @author Firok
+ * */
+public class RegexPipeline
+{
+	public String replaceFirst(String content, String regex, String replacement)
+	{
+		return getPattern(regex)
+				.matcher(content)
+				.replaceFirst(replacement);
+	}
+	public String replaceFirst(String content, Map<String, String> mapRegexReplacement)
+	{
+		for(var entryRegexReplacement : mapRegexReplacement.entrySet())
+		{
+			var regex = entryRegexReplacement.getKey();
+			var replacement = entryRegexReplacement.getValue();
+			content = replaceFirst(content, regex, replacement);
+		}
+		return content;
+	}
+
+	public String replaceAll(String content, String regex, String replacement)
+	{
+		return getPattern(regex)
+				.matcher(content)
+				.replaceAll(replacement);
+	}
+	public String replaceAll(String content, Map<String, String> mapRegexReplacement)
+	{
+		for(var entryRegexReplacement : mapRegexReplacement.entrySet())
+		{
+			var regex = entryRegexReplacement.getKey();
+			var replacement = entryRegexReplacement.getValue();
+			content = replaceAll(content, regex, replacement);
+		}
+		return content;
+	}
+
+	// 正则缓存
+	private final Map<String, Pattern> mapCachedPattern = new ConcurrentHashMap<>();
+	public Pattern getPattern(String regex)
+	{
+		return mapCachedPattern.computeIfAbsent(regex, Pattern::compile);
+	}
+	public void cleanPatterns()
+	{
+		mapCachedPattern.clear();
+	}
+}
