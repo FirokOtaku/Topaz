e51ff81fa5ce5bc144d66ef48cf4dbdd805fe323
Firok <firok@qq.com>
2022-09-05 10:29:29 +0800

    3.10.0

--- src/main/java/firok/topaz/Topaz.java
+++ src/main/java/firok/topaz/Topaz.java
@@ -3,7 +3,7 @@ package firok.topaz;
 public final class Topaz
 {
 	public static final String NAME = "Topaz";
-	public static final String VERSION = "3.8.0";
+	public static final String VERSION = "3.10.0";
 	public static final String AUTHOR = "Firok";
 	public static final String URL = "https://github.com/351768593/Topaz";
 
