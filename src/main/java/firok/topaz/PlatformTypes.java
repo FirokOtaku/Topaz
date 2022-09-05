a115fc32a8b57d2545d3e3697941f032067716b9
Firok <firok@qq.com>
2022-09-05 09:47:55 +0800

    3.9.0

---
+++ src/main/java/firok/topaz/PlatformTypes.java
@@ -0,0 +1,28 @@
+package firok.topaz;
+
+public class PlatformTypes
+{
+	public static final PlatformType Unknown = new PlatformType.Unknown();
+
+	public static final PlatformType Windows = new PlatformType.Windows();
+
+	@Deprecated
+	public static final PlatformType Linux = new PlatformType.Linux();
+
+	@Deprecated
+	public static final PlatformType MacOS = new PlatformType.MacOS();
+
+	public static final PlatformType Current;
+	static
+	{
+		var current = Unknown;
+		for(var type : new PlatformType[]{ Windows, Linux, MacOS })
+		{
+			if(type.isCurrent())
+			{
+				current = type;
+			}
+		}
+		Current = current;
+	}
+}
