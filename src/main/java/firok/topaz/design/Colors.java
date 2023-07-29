a90d091ef80d3d8e0182a5418eefe9870b99c742
FirokOtaku <351768593@outlook.com>
2023-07-29 15:07:25 +0800

    5.4.0

--- src/main/java/firok/topaz/design/Colors.java
+++ src/main/java/firok/topaz/design/Colors.java
@@ -1,5 +1,7 @@
 package firok.topaz.design;
 
+import firok.topaz.math.Maths;
+
 import java.awt.*;
 
 /**
@@ -29,5 +31,80 @@ public final class Colors
 	public static int getColorRGBBetween(int c1,int c2)
 	{
 		return getColorBetween(new Color(c1),new Color(c2)).getRGB();
+	}
+
+	/**
+	 * 计算色彩饱和度
+	 * @params r,g,b 介于 0 到 1 之间的 RGB 通道量
+	 * @since 5.4.0
+	 * @author Firok
+	 * */
+	public static float saturationOf(float r, float g, float b)
+	{
+		float max = Maths.max(r, g, b), min = Maths.min(r, g, b);
+		return 1 - min / max;
+	}
+
+	/**
+	 * 计算色彩亮度
+	 * @params r,g,b 介于 0 到 1 之间的 RGB 通道量
+	 * @since 5.4.0
+	 * @author Firok
+	 * */
+	public static float lightnessOf(float r, float g, float b)
+	{
+		return r * 0.299F + g * 0.587F + b * 0.114F;
+	}
+
+	/**
+	 * 计算色彩灰度
+	 * @params r,g,b 介于 0 到 1 之间的 RGB 通道量
+	 * @since 5.4.0
+	 * @author Firok
+	 * */
+	public static float grayOf(float r, float g, float b)
+	{
+		return (r + g + b) / 3;
+	}
+
+	/**
+	 * 计算色彩色相
+	 * <table>
+	 *     <tr>
+	 *         <td></td>
+	 *         <td> 0° - <span style="color: rgb(255,0,0)">R</span> </td>
+	 *         <td></td>
+	 *     </tr>
+	 *     <tr>
+	 *         <td> 300° - <span style="color: rgb(255,0,255)">M</span> </td>
+	 *         <td>  </td>
+	 *         <td> 60° - <span style="color: rgb(255,255,0)">Y</span> </td>
+	 *     </tr>
+	 *     <tr>
+	 *         <td> 240° - <span style="color: rgb(0,0,255)">B</span> </td>
+	 *         <td>  </td>
+	 *         <td> 120° - <span style="color: rgb(0,255,0)">G</span> </td>
+	 *     </tr>
+	 *     <tr>
+	 *         <td></td>
+	 *         <td> 180° - <span style="color: rgb(0,255,255)">C</span> </td>
+	 *         <td></td>
+	 *     </tr>
+	 * </table>
+	 * @params r,g,b 介于 0 到 1 之间的 RGB 通道量
+	 * @return 色相度数
+	 * @since 5.4.0
+	 * @author Firok
+	 * */
+	public static float hueOf(float r, float g, float b)
+	{
+		final float max = Maths.max(r, g, b), min = Maths.min(r, g, b);
+		final float interval = max - min;
+		if(max == min) return 0;
+		else if(max == r && g >= b) return 60F * (g - b) / interval + 0F;
+		else if(max == r && g < b) return 60F * (g - b) / interval + 360F;
+		else if(max == g) return 60F * (b - r) / interval + 120F;
+		else if(max == b) return 60F * (r - g) / interval + 240F;
+		else return Float.NaN; // 虽然应该不会到这里
 	}
 }
