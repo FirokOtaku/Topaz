915d95720649cae6c29e1ffd2e086fbe3f4d45a7
FirokOtaku <351768593@outlook.com>
2024-01-03 23:24:16 +0800

    7.10.0

--- src/main/java/firok/topaz/general/Binaries.java
+++ src/main/java/firok/topaz/general/Binaries.java
@@ -1,9 +1,12 @@
 package firok.topaz.general;
 
+import java.math.BigInteger;
+
 /**
  * @since 3.6.0
  * @author Firok
  * */
+@SuppressWarnings("DanglingJavadoc")
 public final class Binaries
 {
 	private Binaries() { }
@@ -44,6 +47,69 @@ public final class Binaries
 			int b1 = toHexByte(hex.charAt(step * 2)) << 4;
 			int b2 = toHexByte(hex.charAt(step * 2 + 1));
 			ret[step] = (byte) (b1 | b2);
+		}
+		return ret;
+	}
+
+	/**
+	 * 0x20201223 -> "20201223"
+	 * @since 7.10.0
+	 * */
+	public static String toDirectString(byte[] bytes)
+	{
+		var ret = new StringBuilder();
+		for(byte b : bytes)
+		{
+			int num = Byte.toUnsignedInt(b);
+			if(num < 10) ret.append('0');
+			ret.append(num);
+		}
+		return ret.toString();
+	}
+	/**
+	 * "20201223" -> 0x20201223
+	 * @since 7.10.0
+	 * */
+	public static byte[] toDirectByte(String str)
+	{
+		var len = str.length() / 2;
+		var ret = new byte[len];
+		for(int step = 0; step < len; step ++)
+		{
+			ret[step] = (byte) (Character.digit(str.charAt(step * 2), 16) * 10 + Character.digit(str.charAt(step * 2 + 1), 16));
+		}
+		return ret;
+	}
+
+	/**
+	 * 低字节序, 数据转十进制
+	 * @since 7.10.0
+	 * */
+	public static BigInteger toBinLSBValue(byte[] bytes)
+	{
+		var ret = BigInteger.ZERO;
+		for(int step = bytes.length - 1; step >= 0; step--)
+		{
+			var b = bytes[step];
+//			ret <<= 8;
+//			ret |= Byte.toUnsignedInt(b);
+			ret = ret.shiftLeft(8).add(BigInteger.valueOf(Byte.toUnsignedInt(b)));
+		}
+		return ret;
+	}
+	/**
+	 * 低字节序, 十进制转数据
+	 * @since 7.10.0
+	 * */
+	public static byte[] toBinLSBByte(BigInteger num, int length)
+	{
+		var ret = new byte[length];
+		for(int step = 0; step < length; step++)
+		{
+//			ret[step] = (byte) (num & 0xFF);
+//			num >>>= 8;
+			ret[step] = num.and(BigInteger.valueOf(0xFF)).byteValue();
+			num = num.shiftRight(8);
 		}
 		return ret;
 	}
--- src/test/java/firok/topaz/test/BinariesTests.java
+++ src/test/java/firok/topaz/test/BinariesTests.java
@@ -4,6 +4,8 @@ import firok.topaz.general.Binaries;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigInteger;
+
 public class BinariesTests
 {
 
@@ -26,5 +28,39 @@ public class BinariesTests
 		testHexOne("AF", new int[] { 0xAF });
 		testHexOne("A452", new int[] { 0xA4, 0x52 });
 		testHexOne("A45254", new int[] { 0xA4, 0x52, 0x54 });
+	}
+
+	void testBinOne(int raw)
+	{
+		var value = BigInteger.valueOf(raw);
+
+		var buffer = Binaries.toBinLSBByte(value, 4);
+		var value2 = Binaries.toBinLSBValue(buffer);
+		Assertions.assertEquals(value, value2);
+	}
+	@Test
+	void testBinConvert()
+	{
+		for(var step = 0; step < 100; step++)
+		{
+			testBinOne(step);
+		}
+
+		Assertions.assertArrayEquals(
+				new byte[] {},
+				Binaries.toBinLSBByte(BigInteger.ZERO, 0)
+		);
+		Assertions.assertArrayEquals(
+				new byte[] { 0x00, 0x04, 0x00, 0x00 },
+				Binaries.toBinLSBByte(BigInteger.valueOf(1024), 4)
+		);
+		Assertions.assertArrayEquals(
+				new byte[] { 0x18, (byte) 0x90, 0x01, 0x00 },
+				Binaries.toBinLSBByte(BigInteger.valueOf(102424), 4)
+		);
+		Assertions.assertArrayEquals(
+				new byte[] { (byte) 0xBC, 0x44, 0x00, 0x00 },
+				Binaries.toBinLSBByte(BigInteger.valueOf(17596), 4)
+		);
 	}
 }
